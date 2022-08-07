0878b6c6447cb3abe0e42b04f24576222c1b5c04
Jerome Cho <yourname@example.com>
2022-08-06 17:12:02 -0700

    Add files and config for heroku deployment

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// TODO - try!
-//  app.use('/users', usersRouter);
-//  before app.use('/', ...)
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
